b0ffb757fe6675b4648524a6de073359d319803e
Engramar Bollas <engramar.bollas@gmail.com>
2022-03-13 19:18:28 +1100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
-import "react-image-gallery/styles/css/image-gallery.css";
-import ImageGallery from "react-image-gallery";
+import { render } from "react-dom";
+import Gallery from "react-photo-gallery";
+import { photos } from "./photos";
 
-const ImagesGallery = () => {
-  const [images, setImages] = React.useState(null);
-
-  React.useEffect(() => {
-    let shouldCancel = false;
-
-    const call = async () => {
-      const response = await axios.get(
-        "https://photos.app.goo.gl/L8S7Fzhch4U14w4c6"
-      );
-      if (!shouldCancel && response.data && response.data.length > 0) {
-        setImages(
-          response.data.map(url => ({
-            original: `${url}=w1024`,
-            thumbnail: `${url}=w100`
-          }))
-        );
-      }
-    };
-    call();
-    return () => (shouldCancel = true);
-  }, []);
-
-  return images ? <ImageGallery items={images} /> : null;
-};
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<ImagesGallery />, rootElement);
+/* popout the browser and maximize to see more rows! -> */
+const BasicRows = () => <Gallery photos={photos} />;
+render(<BasicRows />, document.getElementById("app"));
---
+++ src/photos.js
@@ -0,0 +1,243 @@
+export const photos = [
+    {
+      src: "https://user-images.githubusercontent.com/7553347/158049657-9ca57b12-529c-4e0d-a868-1fc6beb62679.png",
+      width: 3,
+      height: 3 
+    },
+    {
+      src: "https://user-images.githubusercontent.com/7553347/158049699-0ec8a040-d979-4880-af2f-b48bfe1c6789.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://user-images.githubusercontent.com/7553347/158049701-7dd23b4a-7e85-4cfc-b41f-9561180549b0.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://user-images.githubusercontent.com/7553347/158049702-3fde9eff-d0d0-41b6-bd5f-d2d22d3bce05.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://user-images.githubusercontent.com/7553347/158049698-89a015c6-99dd-424b-91ea-a25e15008e72.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://user-images.githubusercontent.com/7553347/158049825-83a44ba1-8fee-4637-97d7-774745300785.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+        src: "https://user-images.githubusercontent.com/7553347/158049657-9ca57b12-529c-4e0d-a868-1fc6beb62679.png",
+        width: 3,
+        height: 3 
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049699-0ec8a040-d979-4880-af2f-b48bfe1c6789.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049701-7dd23b4a-7e85-4cfc-b41f-9561180549b0.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049702-3fde9eff-d0d0-41b6-bd5f-d2d22d3bce05.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049698-89a015c6-99dd-424b-91ea-a25e15008e72.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049825-83a44ba1-8fee-4637-97d7-774745300785.jpg",
+        width: 4,
+        height: 3
+      },            
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049657-9ca57b12-529c-4e0d-a868-1fc6beb62679.png",
+        width: 3,
+        height: 3 
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049699-0ec8a040-d979-4880-af2f-b48bfe1c6789.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049701-7dd23b4a-7e85-4cfc-b41f-9561180549b0.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049702-3fde9eff-d0d0-41b6-bd5f-d2d22d3bce05.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049698-89a015c6-99dd-424b-91ea-a25e15008e72.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049825-83a44ba1-8fee-4637-97d7-774745300785.jpg",
+        width: 4,
+        height: 3
+      },            
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049657-9ca57b12-529c-4e0d-a868-1fc6beb62679.png",
+        width: 3,
+        height: 3 
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049699-0ec8a040-d979-4880-af2f-b48bfe1c6789.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049701-7dd23b4a-7e85-4cfc-b41f-9561180549b0.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049702-3fde9eff-d0d0-41b6-bd5f-d2d22d3bce05.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049698-89a015c6-99dd-424b-91ea-a25e15008e72.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049825-83a44ba1-8fee-4637-97d7-774745300785.jpg",
+        width: 4,
+        height: 3
+      },            
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049657-9ca57b12-529c-4e0d-a868-1fc6beb62679.png",
+        width: 3,
+        height: 3 
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049699-0ec8a040-d979-4880-af2f-b48bfe1c6789.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049701-7dd23b4a-7e85-4cfc-b41f-9561180549b0.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049702-3fde9eff-d0d0-41b6-bd5f-d2d22d3bce05.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049698-89a015c6-99dd-424b-91ea-a25e15008e72.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049825-83a44ba1-8fee-4637-97d7-774745300785.jpg",
+        width: 4,
+        height: 3
+      },            
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049657-9ca57b12-529c-4e0d-a868-1fc6beb62679.png",
+        width: 3,
+        height: 3 
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049699-0ec8a040-d979-4880-af2f-b48bfe1c6789.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049701-7dd23b4a-7e85-4cfc-b41f-9561180549b0.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049702-3fde9eff-d0d0-41b6-bd5f-d2d22d3bce05.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049698-89a015c6-99dd-424b-91ea-a25e15008e72.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049825-83a44ba1-8fee-4637-97d7-774745300785.jpg",
+        width: 4,
+        height: 3
+      },            
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049657-9ca57b12-529c-4e0d-a868-1fc6beb62679.png",
+        width: 3,
+        height: 3 
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049699-0ec8a040-d979-4880-af2f-b48bfe1c6789.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049701-7dd23b4a-7e85-4cfc-b41f-9561180549b0.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049702-3fde9eff-d0d0-41b6-bd5f-d2d22d3bce05.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049698-89a015c6-99dd-424b-91ea-a25e15008e72.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049825-83a44ba1-8fee-4637-97d7-774745300785.jpg",
+        width: 4,
+        height: 3
+      },            
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049657-9ca57b12-529c-4e0d-a868-1fc6beb62679.png",
+        width: 3,
+        height: 3 
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049699-0ec8a040-d979-4880-af2f-b48bfe1c6789.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049701-7dd23b4a-7e85-4cfc-b41f-9561180549b0.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049702-3fde9eff-d0d0-41b6-bd5f-d2d22d3bce05.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049698-89a015c6-99dd-424b-91ea-a25e15008e72.jpg",
+        width: 4,
+        height: 3
+      },
+      {
+        src: "https://user-images.githubusercontent.com/7553347/158049825-83a44ba1-8fee-4637-97d7-774745300785.jpg",
+        width: 4,
+        height: 3
+      }                  
+  ];
+  
